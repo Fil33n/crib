8bb8a780307b68ab352fdda42a230172c97b7cb2
Fil33n <96204813+Fil33n@users.noreply.github.com>
2021-12-16 17:51:34 +0300

    Update simple_calc.py

--- simple_calc.py
+++ simple_calc.py
@@ -2,27 +2,27 @@ a = float(input())
 b = float(input())
 o = input()
 if o == '+':
-    print(a+b)
+    print(a + b)
 elif o == '-':
-    print(a-b)
+    print(a - b)
 elif o == '/':
     if b == 0.0:
         print('Деление на 0!')
     else:
-        print(a/b)
+        print(a / b)
 elif o == '*':
-    print(a*b)
+    print(a * b)
 elif o == 'mod':
     if b == 0.0:
         print('Деление на 0!')
     else:
-        print(a%b)
+        print(a % b)
 elif o == 'pow':
-    print(a**b)
+    print(a ** b)
 elif o == 'div':
     if b == 0.0:
         print('Деление на 0!')
     else:
-        print(a//b)
+        print(a // b)
 else:
     print('Шёл в зад с такой функцией')
